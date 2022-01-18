7c72ac69a6b696173ede49029e59ae20ff777a74
Adrien <adrienlcp@gmail.com>
2022-01-18 10:15:33 +0100

    fail to make a modal component, so make it manually every time... Easter Egg done

--- src/components/EasterEgg/index.jsx
+++ src/components/EasterEgg/index.jsx
@@ -2,12 +2,31 @@ import React from 'react';
 
 import './styles.scss';
 
-const EasterEgg = () => {
+const EasterEgg = ({ setShowEasterEgg }) => {
 
   return (
-    <div className='easter_egg'>
-      
-    </div>
+    <section className='easter-egg'>
+
+      <div class="pyro">
+        <div class="before"></div>
+        <div class="after"></div>
+      </div>
+
+      <p className='easter-egg__container'>
+        Félicitations, vous avez débloqué le secret du site !
+        <a
+          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+          className="easter-egg__container__button"
+          rel="noreferrer"
+          target="_blank"
+          onClick={() => {
+            setShowEasterEgg(false);
+          }}
+        >
+          Cliquez ici
+        </a>
+      </p>
+    </section>
   );
 };
 
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,94 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import EasterEgg from '../../components/EasterEgg';
 
 import './styles.scss';
 
+import blackSnippet from '../../media/icons/snippet_black.svg';
+import whiteSnippet from '../../media/icons/snippet_white.svg';
+
 const Home = () => {
+
+  const [toggleSnippet1, setToggleSnippet1] = useState(true);
+  const [toggleSnippet2, setToggleSnippet2] = useState(true);
+  const [toggleSnippet3, setToggleSnippet3] = useState(true);
+  const [toggleSnippet4, setToggleSnippet4] = useState(true);
+  const [toggleSnippet5, setToggleSnippet5] = useState(true);
+  const [showEasterEgg, setShowEasterEgg] = useState(false);
+
+  useEffect(() => {
+    document.title = "Adrien Lacourpaille";
+  }, []);
+
+  useEffect(() => {
+    if (
+      toggleSnippet1 === false &&
+      toggleSnippet2 === false &&
+      toggleSnippet3 === false &&
+      toggleSnippet4 === false &&
+      toggleSnippet5 === false
+    ) {
+      setShowEasterEgg(true);
+    };
+  }, [toggleSnippet1, toggleSnippet2, toggleSnippet3, toggleSnippet4, toggleSnippet5]);
 
   return (
     <div className='home'>
+      <header className='home__header'>
+        <h1 className='home__header__title'>
+          Adrien Lacourpaille
+        </h1>
+
+        <h2 className='home__header__subtitle'>
+          Développeur Front
+        </h2>
+      </header>
+
+      <img
+        src={toggleSnippet1 ? blackSnippet : whiteSnippet}
+        alt="Snippets de code"
+        className="home__icon1"
+        onClick={() => {
+          setToggleSnippet1(!toggleSnippet1);
+        }}
+      />
+      <img
+        src={toggleSnippet2 ? blackSnippet : whiteSnippet}
+        alt="Snippets de code"
+        className="home__icon2"
+        onClick={() => {
+          setToggleSnippet2(!toggleSnippet2);
+        }}
+      />
+      <img
+        src={toggleSnippet3 ? blackSnippet : whiteSnippet}
+        alt="Snippets de code"
+        className="home__icon3"
+        onClick={() => {
+          setToggleSnippet3(!toggleSnippet3);
+        }}
+      />
+      <img
+        src={toggleSnippet4 ? blackSnippet : whiteSnippet}
+        alt="Snippets de code"
+        className="home__icon4"
+        onClick={() => {
+          setToggleSnippet4(!toggleSnippet4);
+        }}
+      />
+      <img
+        src={toggleSnippet5 ? blackSnippet : whiteSnippet}
+        alt="Snippets de code"
+        className="home__icon5"
+        onClick={() => {
+          setToggleSnippet5(!toggleSnippet5);
+        }}
+      />
+      { showEasterEgg && (
+        <EasterEgg
+          setShowEasterEgg={setShowEasterEgg}
+        />
+      )}
     </div>
   );
 };
